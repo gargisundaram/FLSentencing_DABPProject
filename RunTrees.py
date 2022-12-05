01eaaede9f66a4451a1819afeb51abf90d57dc00
Gargi Sundaram <gargis@andrew.cmu.edu>
2022-12-04 20:24:47 -0500

    add plot save feature

--- RunTrees.py
+++ RunTrees.py
@@ -3,7 +3,7 @@
 #model is the type of sklearn model being run (ex: DecisionTreeClassifier)
 #should change the random state but it's currently set to a default
 
-def get_tree(df, target, model, paramdict, seed = 10):
+def get_tree(df, target, model, paramdict, importanceplotfile, shapplotfile, seed = 10):
   #split into x and y variables
   x = df.drop([target], axis = 1)
   y = df[[target]]
@@ -16,12 +16,12 @@ def get_tree(df, target, model, paramdict, seed = 10):
   #get best params
   tree = model(random_state = seed)
   gs = GridSearchCV(model(), paramdict)
-  grid_result = gs.fit(x_train, y_train.values.ravel())
+  grid_result = gs.fit(x_train, y_train)
   best_params = grid_result.best_params_
   
   #run model
   tree = model(**best_params, random_state=seed)
-  t = tree.fit(x_train, y_train.values.ravel())
+  t = tree.fit(x_train, y_train)
   y_pred_train = t.predict(x_train)
   y_pred_test = t.predict(x_test)
 
@@ -34,10 +34,13 @@ def get_tree(df, target, model, paramdict, seed = 10):
     print(metric['conf_mat'],
           metric['class_report'],
           metric['accuracy'])
+    out = 'predict_proba'
+
   elif (model == DecisionTreeRegressor) or (model == RandomForestRegressor) or (model == XGBRegressor):
     metric = {'train_rmse' : np.sqrt(mean_squared_error(y_train, y_pred_train)), 
               'test_rmse' : np.sqrt(mean_squared_error(y_test, y_pred_test))}
-    print('RMSE:', metric['rmse'])
+    print('train RMSE:', metric['train_rmse'], '\ntest RMSE:', metric['test_rmse'])
+    out = "raw"
 
   #feature importance
   importance = pd.DataFrame(tree.feature_importances_, columns = ["Importance"])
@@ -45,15 +48,22 @@ def get_tree(df, target, model, paramdict, seed = 10):
   importance = importance[importance['Importance'] > 0].sort_values(by = 'Importance', ascending = False)
 
   # Print table of feature importances
-  display(importance)
+  #display(importance)
 
   # Histogram of feature importances
   plt.bar(importance['Features'], importance['Importance'])
   plt.xticks(rotation = -90)
   plt.show()
+  plt.savefig(importanceplotfile)
 
   #Tree plot
   #plt.figure(figsize=(25,15))
   #plot_tree(tree, feature_names = x.columns, filled = True)
+
+  explainer = shap.TreeExplainer(t)
+  shap_values = explainer.shap_values(x_test)
+  shap.summary_plot(shap_values, x_test)
+  plt.savefig(shapplotfile)
   
-  return t, metric, importance
+  return t, metric, importance, shap_values
+
